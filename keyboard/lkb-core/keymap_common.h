7ed0e3dd2c9a3952e80865b5c79b8a60e77cd861
tsxx11 <80495902+tsxx11@users.noreply.github.com>
2021-11-10 19:58:48 +0800

    Update keymap_common.h

--- keyboard/lkb-core/keymap_common.h
+++ keyboard/lkb-core/keymap_common.h
@@ -34,7 +34,7 @@ extern const action_t fn_actions[];
 	K200,       K202, K203, K204, K205, K206, K207, K208, K209, K20A, K20B, K20C, K20D, K20E, K20F, \
 	K300,       K302, K303, K304, K305, K306, K307, K308, K309, K30A, K30B, K30C,       K30E, K30F, \
 	      K401, K402, K403, K404, K405, K406, K407, K408, K409, K40A, K40B,       K40D, K40E, K40F, \
-	K500, K501, K502, K503,       K505,       K507,       K509, K50A, K50B,       K50D, K50E, K50F
+	K500, K501, K502, K503,       K505,       K507,       K509, K50A, K50B,       K50D, K50E, K50F  \
 ) { \
 	{ K000,   K001,   K002,   K003,   K004,   K005,   K006,   K007,   K008,   K009,   K00A,   K00B,   K00C,   KC_NO,   K00E,   K00F }, \
 	{ K200,   KC_NO,   K202,   K203,   K204,   K205,   K206,   K207,   K208,   K209,   K20A,   K20B,   K20C,   K20D,   K20E,   K20F }, \
